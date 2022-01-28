29f83967e2ff6de3849ae61fbaecb23169371e9a
Wesly <weslyramalho@yahoo.com.br>
2022-01-28 15:33:01 -0300

    Class program criada

--- src/db/DB.java
+++ src/db/DB.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DB {
@@ -12,11 +13,26 @@ public class DB {
 	
 	public static Connection getConnection() {
 		if (conn == null) {
-			Properties props = loadProperties();
-			String url = props.getProperty("dburl");
-			conn = DriverManager.getConnection(url, props);
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
 		}
+			}
 		return conn;
+	}
+	
+	public static void closeConnection() {
+		if(conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
 	}
 	
 	
