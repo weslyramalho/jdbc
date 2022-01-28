ea1ea67441dc205a7ada6b92e1583fe7799a7960
Wesly <weslyramalho@yahoo.com.br>
2022-01-28 15:27:59 -0300

    Criado conexao com banco de dados

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,9 +2,23 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Properties;
 
 public class DB {
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		if (conn == null) {
+			Properties props = loadProperties();
+			String url = props.getProperty("dburl");
+			conn = DriverManager.getConnection(url, props);
+		}
+		return conn;
+	}
+	
 	
 	private static Properties loadProperties() {
 		try (FileInputStream fs = new FileInputStream("db.properties")){
